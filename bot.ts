f9c9664fe4433e293067dbd9f89fe46321e4e188
OfMikeAndMen <kinqmike@gmail.com>
2020-08-01 16:08:14 +0200

    ts init

---
+++ bot.ts
@@ -0,0 +1,91 @@
+require("dotenv").config();
+if(!process.env.d_TOKEN) {
+    throw new Error("no discord token set");
+}
+
+// import { Client } from "eris";
+import { Client } from 'eris'
+// import fs from 'fs';
+// let cmds = require("./cmds.json");
+
+let bot: Client = new Client(process.env.d_TOKEN, {
+    "allowedMentions": { "everyone": true },
+    "autoreconnect": true
+});
+
+bot.on("ready", () => {
+    console.log(new Date() + " - bot ready");
+});
+
+bot.on("messageCreate", () => { // When a message is created
+
+    // if (msg.channel.type == 0 && msg.member) {
+
+    //     let serverID = msg.member.guild.id;
+    //     let messageID = msg.id;
+    //     let channelID = msg.channel.id;
+    //     let userID = msg.member.id;
+    //     let message = msg.content;
+
+    //     if (message.substring(0, 1) == '!') {
+    //         let args = message.substring(1).split(' ');
+    //         let cmd = args.shift();
+
+    //         switch (cmd) {
+
+    //             case "wtf":
+    //                 if (channelID === "735315394151055491") { //just to show how 2 post images so limited to #test
+    //                     bot.sendChannelTyping(channelID);
+    //                     const body = fs.readFileSync("./images/headpat1.png");
+    //                     bot.createMessage(channelID, undefined, { file: body, name: "headpat1.png" });
+    //                 }
+    //                 break;
+
+    //             case "cmds": //lists all commands in cmds.json
+    //                 bot.createMessage(channelID, Object.keys(cmds).join(" "));
+    //                 break;
+
+    //             case "addcmd":
+    //                 if (channelID === "735315394151055491" && args[0] && args[1]) { //only allow adding commands from #test
+    //                     let newCmd = args.shift();
+    //                     if (cmds[newCmd] === undefined) { //dont add command if it exists already
+    //                         cmds[newCmd] = args.join(" ");
+    //                         bot.createMessage(channelID, "Added command: " + newCmd)
+    //                         fs.writeFile("./cmds.json", JSON.stringify(cmds, null, 2), () => { })
+    //                     }
+    //                 }
+    //                 break;
+
+    //             case "editcmd":
+    //                 if (channelID === "735315394151055491" && args[0] && args[1]) { //only allow editing commands from #test
+    //                     let newCmd = args.shift();
+    //                     if (cmds[newCmd] !== undefined) { //make sure command exists
+    //                         cmds[newCmd] = args.join(" ");
+    //                         bot.createMessage(channelID, "Edited command: " + newCmd)
+    //                         fs.writeFile("./cmds.json", JSON.stringify(cmds, null, 2), () => { })
+    //                     }
+    //                 }
+    //                 break;
+
+    //             case "deletecmd":
+    //                 if (channelID === "735315394151055491" && args[0]) { //only allow editing commands from #test
+    //                     let newCmd = args.shift();
+    //                     if (cmds[newCmd] !== undefined) { //make sure command exists
+    //                         delete cmds[newCmd]; //delete command
+    //                         bot.createMessage(channelID, "Deleted command: " + newCmd)
+    //                         fs.writeFile("./cmds.json", JSON.stringify(cmds, null, 2), () => { })
+    //                     }
+    //                 }
+    //                 break;
+
+    //             default:
+    //                 if (cmds[cmd]) {
+    //                     bot.createMessage(channelID, cmds[cmd])
+    //                 }
+    //                 break;
+    //         }
+    //     }
+    // }
+});
+
+bot.connect(); // Get the bot to connect to Discord
